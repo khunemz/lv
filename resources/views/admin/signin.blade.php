77cf2d3c36fe74d8838459f9abb3ba306f2e22e1
khunemz <khunemz@gmail.com>
2016-03-09 14:38:05 +0700

    Create AdminMiddleware and Register in Kernel

--- resources/views/admin/signin.blade.php
+++ resources/views/admin/signin.blade.php
@@ -5,6 +5,7 @@
         <h4>Sign in</h4>
         <input name="email" type="text" class="form-control" placeholder="Email"/>
         <input name="password" class="form-control" type="password" placeholder="Password"/>
+        <input type="checkbox" class="checkbox-inline" /> Remember me
         <button class="btn btn-default btn-block">Sign in</button>
         <input type="hidden" name="_token" value="{{ csrf_token() }}" />
     </form>
