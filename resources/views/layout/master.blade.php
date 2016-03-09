7dda0e88e3cdd036a637bf21b7d5540e0a4d6b09
khunemz <khunemz@gmail.com>
2016-03-09 11:10:45 +0700

    Modify Home page, Test:green

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>This is a Chutipong's website</title>
+    <meta name="description" content="chutipong Website">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    {!! Html::style('lib/bootstrap/dist/css/bootstrap.min.css') !!}
+    {!! Html::style('lib/bootstrap/dist/css/bootstrap-theme.min.css') !!}
+    {!! Html::style('css/bootstrap-custom-theme.css') !!}
+    {!! Html::style('css/main.css') !!}
+</head>
+<body>
+<!--[if lt IE 8]>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+<div class="container">
+    @yield('content')
+</div> <!-- /container -->
+
+{!! Html::script('lib/jquery/dist/jquery.min.js') !!}
+{!! Html::script('lib/bootstrap/dist/js/bootstrap.min.js') !!}
+{!! Html::script('js/main.js') !!}
+
+</body>
+</html>
---
+++ tests/UserTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     *
+    /** @test **/
+    public function should_show_welcome()
+    {
+        $this->visit('/')->see('welcome');
+    }
+}
